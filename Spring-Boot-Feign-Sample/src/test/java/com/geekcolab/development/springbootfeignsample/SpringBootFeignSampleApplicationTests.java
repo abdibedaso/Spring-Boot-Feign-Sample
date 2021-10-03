d40b6e7076880ba45379314c5f339cfa00ed1665
Abdi T Bedaso <atbedaso@gmail.com>
2021-10-03 14:48:06 -0400

    Add service starter

---
+++ Spring-Boot-Feign-Sample/src/test/java/com/geekcolab/development/springbootfeignsample/SpringBootFeignSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekcolab.development.springbootfeignsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFeignSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
