dea9de336ac43dff9e12284e2225266675c486ea
Abdi T Bedaso <atbedaso@gmail.com>
2021-10-03 14:44:58 -0400

    Create simple spring boot apps

---
+++ AccountServiceApplication/src/test/java/com/geekcolab/development/accountserviceapplication/AccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekcolab.development.accountserviceapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
